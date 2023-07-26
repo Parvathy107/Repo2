0d3d717399460e45768b3b1831caeab598237c0e
MIDHUN M <MIDHUN M@LAPTOP-BGGC3FN3>
2023-07-26 11:48:09 +0530

    Hello

---
+++ Hello1/src/main/java/helloo/Hello3.java
@@ -0,0 +1,22 @@
+package helloo;
+
+public class Hello3 {
+
+	
+		public static void show(int a, int b)
+		{
+			System.out.println(a+" "+b);
+		}
+		public static void show(int a, String b)
+		{
+			System.out.println(a+" "+b);
+		}
+		public static void main(String[] args) {
+			show(3,4);
+			show(7,"Arya");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
